52519d5deb235e5ab6553ca1c784b1243348212e
Bulat <bulatsafiullin2612@gmail.com>
2024-10-26 00:43:37 +0300

    edit WebSocketConfig

--- src/main/java/com/digital/silaai_integartion_service/configs/WebSocketConfig.java
+++ src/main/java/com/digital/silaai_integartion_service/configs/WebSocketConfig.java
@@ -5,10 +5,20 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -51,6 +61,23 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         return false;
     }
 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    Authentication user = SecurityContextHolder.getContext().getAuthentication();
+                    accessor.setUser(user);
+                }
+                return message;
+            }
+        });
+    }
+
+    @Override
     public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
         registration.setMessageSizeLimit(1024 * 1024);
         registration.setSendBufferSizeLimit(1024 * 1024);
@@ -64,5 +91,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         container.setMaxSessionIdleTimeout(600000L);
         return container;
     }
-
 }
